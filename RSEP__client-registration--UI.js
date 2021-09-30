99af7ad3c0b997b97eb02aeb7b28a50bb39b57fa
Henry James Cinco <henryjamescinco@yahoo.com>
2021-09-30 16:20:18 +0800

    reduced javascript lines

--- RSEP__client-registration--UI.js
+++ RSEP__client-registration--UI.js
@@ -1,27 +1,39 @@
-function tab_selected(li_id){
+const currentPage = "personal-information";
+
+function tabSelected(li_id){
+
+    document.getElementById("personal-information").className = "";
+    document.getElementById("payment-information").className = "";
+    document.getElementById("financial-goals").className = "";
+    document.querySelector(".client-registration__form__personal-information").style.display = "none";
+    document.querySelector(".client-registration__form__payment-information").style.display = "none";
+    document.querySelector(".client-registration__form__financial-goals").style.display = "none";
+
+    document.getElementById(li_id).className = "client-registration__nav--active";
+
     if(li_id=="personal-information"){
-        document.getElementById(li_id).className = "client-registration__nav--active";
-        document.getElementById("payment-information").className = "";
-        document.getElementById("financial-goals").className = "";
         document.querySelector(".client-registration__form__personal-information").style.display = "flex";
-        document.querySelector(".client-registration__form__payment-information").style.display = "none";
-        document.querySelector(".client-registration__form__financial-goals").style.display = "none";
+        currentPage = li_id;
     }
-    if(li_id=="payment-information"){
-        document.getElementById("personal-information").className = "";
-        document.getElementById(li_id).className = "client-registration__nav--active";
-        document.getElementById("financial-goals").className = "";
-        document.querySelector(".client-registration__form__personal-information").style.display = "none";
+    else if(li_id=="payment-information"){
         document.querySelector(".client-registration__form__payment-information").style.display = "flex";
-        document.querySelector(".client-registration__form__financial-goals").style.display = "none";
+        currentPage = li_id;
     }
-    if(li_id=="financial-goals"){
-        document.getElementById("personal-information").className = "";
-        document.getElementById("payment-information").className = "";
-        document.getElementById(li_id).className = "client-registration__nav--active";
-        document.querySelector(".client-registration__form__personal-information").style.display = "none";
-        document.querySelector(".client-registration__form__payment-information").style.display = "none";
+    else if(li_id=="financial-goals"){
         document.querySelector(".client-registration__form__financial-goals").style.display = "flex";
+        currentPage = li_id;
     }
-    
+
+}
+
+function nextPage(e){
+    e.preventDefault();
+    if(currentPage == "personal-information"){
+
+        tabSelected("payment-information");
+        console.log("next");
+    }
+    else if(currentPage == "payment"){
+        tabSelected("financial-goals");
+    }
 }
